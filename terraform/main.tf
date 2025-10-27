afe8722461e77b24d9b2301196fff593e4199602
Basavaraj Gudageri <42531978+BasavarajGudageri-05@users.noreply.github.com>
2025-10-27 12:39:56 +0530

    resolved error

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,12 +13,12 @@ resource "aws_instance" "instance-1" {
 }
 
 resource "aws_instance" "instance-2" {
-  ami           = "ami-0fff1b9a61dec8a5f"
-  instance_type = "t2.micro"
+  ami           = "ami-02d26659fd82cf299"
+  instance_type = "t3.micro"
   security_groups = ["default"]
-  key_name = "project"
+  key_name = "Keypari4all"
   tags ={
-    Name = "node-expo"
+    Name = "instance-2"
   }
 }
 
